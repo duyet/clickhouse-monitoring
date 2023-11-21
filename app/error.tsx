aba3ce470d9f73992647e5b528ce48270b3e2575
Duyet Le <me@duyet.net>
2023-11-21 17:02:42 +0700

    feat: error handdling

---
+++ app/error.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useEffect } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <h2 className="font-medium">Something went wrong!</h2>
+      <p className="mb-5 font-light">Checking console for more details.</p>
+      <Button
+        variant="outline"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  )
+}
