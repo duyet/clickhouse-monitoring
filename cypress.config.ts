c093dd2f3ebfcc06f6e0c0874f788e5427c6cdee
Duyet Le <me@duyet.net>
2023-11-24 22:02:12 +0700

    chore(ci): cypress cloud

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "8zgiqh",
   defaultCommandTimeout: 15000,
   fixturesFolder: false,
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
-      // include any other plugin code...
+      // tell Cypress to use .babelrc file
+      // and instrument the specs files
+      // only the extra application files will be instrumented
+      // not the spec files themselves
 
       // It's IMPORTANT to return the config object
       // with any changed environment variables
