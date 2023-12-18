ef838930739e534bba02f9f6e2550f679aba4c3a
Duyet Le <me@duyet.net>
2023-12-18 14:23:22 +0700

    chore: remove progress in INSERT query

--- app/[query]/clickhouse-queries.ts
+++ app/[query]/clickhouse-queries.ts
@@ -39,7 +39,7 @@ export const queries: Array<QueryConfig> = [
         formatReadableQuantity(total_rows_approx) as readable_total_rows_approx,
         formatReadableSize(memory_usage) as readable_memory_usage,
         formatReadableSize(peak_memory_usage) as readable_peak_memory_usage,
-        if(total_rows_approx > 0, toString(round((100 * read_rows) / total_rows_approx, 2)) || '%', '') AS progress,
+        if(total_rows_approx > 0  AND query_kind != 'Insert', toString(round((100 * read_rows) / total_rows_approx, 2)) || '%', '') AS progress,
         (elapsed / (read_rows / total_rows_approx)) * (1 - (read_rows / total_rows_approx)) AS estimated_remaining_time
       FROM system.processes
       WHERE is_cancelled = 0 AND query NOT LIKE '%${QUERY_COMMENT}%'
