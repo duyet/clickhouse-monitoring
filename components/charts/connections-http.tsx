722ec065008812d658def70f4d916e9a0fab1821
Duyet Le <me@duyet.net>
2024-05-19 01:04:10 +0700

    chore: add both HTTPConnection and HTTPConnectionsTotal

--- components/charts/connections-http.tsx
+++ components/charts/connections-http.tsx
@@ -12,10 +12,15 @@ export async function ChartConnectionsHttp({
   chartClassName,
 }: ChartProps) {
   const query = `
+    /* HTTPConnection: Number of connections to HTTP server */
+    /* HTTPConnectionsTotal: Total count of all sessions: stored in the pool and actively used right now for http hosts */
+
     SELECT
       ${interval}(event_time) AS event_time,
       SUM(CurrentMetric_HTTPConnection) AS CurrentMetric_HTTPConnection,
-      formatReadableQuantity(CurrentMetric_HTTPConnection) AS readable_CurrentMetric_HTTPConnection
+      formatReadableQuantity(CurrentMetric_HTTPConnection) AS readable_CurrentMetric_HTTPConnection,
+      SUM(CurrentMetric_HTTPConnectionsTotal) AS CurrentMetric_HTTPConnectionsTotal,
+      formatReadableQuantity(CurrentMetric_HTTPConnectionsTotal) AS readable_CurrentMetric_HTTPConnectionsTotal
     FROM system.metric_log
     WHERE event_time >= now() - INTERVAL ${lastHours} HOUR
     GROUP BY event_time
@@ -27,6 +32,8 @@ export async function ChartConnectionsHttp({
       event_time: string
       CurrentMetric_HTTPConnection: number
       readable_CurrentMetric_HTTPConnection: string
+      CurrentMetric_HTTPConnectionsTotal: number
+      readable_CurrentMetric_HTTPConnectionsTotal: string
     }[]
   >({
     query,
@@ -38,8 +45,10 @@ export async function ChartConnectionsHttp({
       <BarChart
         data={data}
         index="event_time"
-        categories={['CurrentMetric_HTTPConnection']}
-        readableColumn="readable_CurrentMetric_HTTPConnection"
+        categories={[
+          'CurrentMetric_HTTPConnectionsTotal',
+          'CurrentMetric_HTTPConnection',
+        ]}
         className={cn('h-52', chartClassName)}
         stack
       />
