9b4abfbe65dbf4be85916525973d708435a2aa40
Duyet Le <me@duyet.net>
2024-05-27 18:01:45 +0700

    feat: engineType() add error handling

--- app/database/[database]/[table]/engine-type.ts
+++ app/database/[database]/[table]/engine-type.ts
@@ -1,15 +1,20 @@
 import { fetchDataWithCache } from '@/lib/clickhouse'
 
 export const engineType = async (database: string, table: string) => {
-  const resp = await fetchDataWithCache()<{ engine: string }[]>({
-    query: `
-      SELECT engine
-      FROM system.tables
-      WHERE (database = {database: String})
-        AND (name = {table: String})
-    `,
-    query_params: { database, table },
-  })
+  try {
+    const resp = await fetchDataWithCache()<{ engine: string }[]>({
+      query: `
+        SELECT engine
+        FROM system.tables
+        WHERE (database = {database: String})
+          AND (name = {table: String})
+      `,
+      query_params: { database, table },
+    })
 
-  return resp?.[0]?.engine || ''
+    return resp?.[0]?.engine || ''
+  } catch (error) {
+    console.error(error)
+    return ''
+  }
 }
