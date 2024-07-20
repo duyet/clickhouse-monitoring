712ea4aa479dd94591f21e7b97dfc107a7a6497f
Duyet Le <me@duyet.net>
2024-07-20 16:56:10 +0700

    chore: remove coverage setup in cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config)
+      // TODO: fix coverage
+      // require('@cypress/code-coverage/task')(on, config)
+
       // tell Cypress to use .babelrc file
       // and instrument the specs files
       // only the extra application files will be instrumented
