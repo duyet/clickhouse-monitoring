4db2111991b6a3ab772fb09ccd854dc35c5a97e0
Duyet Le <me@duyet.net>
2024-09-25 12:27:33 +0700

    fix(test): update jest test

--- lib/clickhouse-query.test.ts
+++ lib/clickhouse-query.test.ts
@@ -4,21 +4,21 @@ import { applyInterval } from './clickhouse-query'
 describe('applyInterval', () => {
   it('should apply toStartOfDay for toStartOfDay interval', () => {
     const result = applyInterval('toStartOfDay', 'myColumn', 'myAlias')
-    expect(result).toEqual('toDate(toStartOfDay(myColumn)) as myAlias') // Change to toEqual
+    expect(result).toEqual('toDate(toStartOfDay(myColumn)) AS myAlias') // Change to toEqual
   })
 
   it('should apply toStartOfWeek for toStartOfWeek interval', () => {
     const result = applyInterval('toStartOfWeek', 'myColumn')
-    expect(result).toEqual('toDate(toStartOfWeek(myColumn)) as myColumn') // Change to toEqual
+    expect(result).toEqual('toDate(toStartOfWeek(myColumn)) AS myColumn') // Change to toEqual
   })
 
   it('should apply toStartOfMonth for toStartOfMonth interval', () => {
     const result = applyInterval('toStartOfMonth', 'myColumn', 'myAlias')
-    expect(result).toEqual('toDate(toStartOfMonth(myColumn)) as myAlias') // Change to toEqual
+    expect(result).toEqual('toDate(toStartOfMonth(myColumn)) AS myAlias') // Change to toEqual
   })
 
   it('should apply toStartOfHour for other intervals', () => {
     const result = applyInterval('toStartOfHour', 'myColumn', 'myAlias')
-    expect(result).toEqual('toStartOfHour(myColumn) as myAlias') // Change to toEqual
+    expect(result).toEqual('toStartOfHour(myColumn) AS myAlias') // Change to toEqual
   })
 })
