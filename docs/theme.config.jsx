51f5e1de4c641f2a940df8d1370aaf65b1bc2744
Duyet Le <me@duyet.net>
2024-11-05 17:57:50 +0700

    chore(docs): add tracking

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,6 +1,9 @@
 export default {
-    logo: <span>ClickHouse Monitoring Docs</span>,
+    logo: <span>ClickHouse Monitoring</span>,
     project: {
       link: 'https://github.com/duyet/clickhouse-monitoring'
-    }
+    },
+    head: (
+      <script async src="https://cdn.seline.so/seline.js" data-token="05046b3773d0534"></script>
+    )
   }
